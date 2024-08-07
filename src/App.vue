3e955f1e08c1118d1703df90892a2a5906451a81
TiagoSpada <tiago.spada.ts@gmail.com>
2024-08-07 15:51:15 +0200

    [upd] Milestone fino alla 4

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,42 @@
-<script></script>
+<script>
+import axios from "axios";
 
-<template><h1>Vue Vitte Template (scss)</h1></template>
+export default {
+	name: "Projects",
+	data() {
+		return {
+			projects: [],
+		};
+	},
+	methods: {
+		getProjects() {
+			const results = axios
+				.get("http://127.0.0.1:8000/api/projects")
+				.then((response) => {
+					if (response.data.status && response.data.results.length) {
+						this.projects = response.data.results;
+						console.log(this.projects);
+					} else {
+						console.log(error);
+					}
+				})
+				.catch((error) => console.log(error));
+		},
+	},
+	created() {
+		this.getProjects();
+	},
+};
+</script>
+
+<template>
+	<header>HEADER</header>
+	<main>
+		<div class="container">
+			<h1>Projects</h1>
+		</div>
+	</main>
+	<footer>FOOTER</footer>
+</template>
 
 <style></style>
